fc4d0b4889615e7b5f1e38a2d73b6e5da0dd29b8
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-09-06 13:14:51 -0500

    feat: added instructions and test cases

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,40 @@
-const { func } = require('../exercise');
+const { inventory } = require('../exercise');
 
-test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
- })
+test('Testing the inventory exercise', () => {
+    expect(inventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]).length).toBe(6);
+    expect(inventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]])).toEqual([[88, "Bowling Ball"], [2, "Dirty Sock"], [3, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]]);
+    expect(inventory([
+        [0, 'Bowling Ball'],
+        [0, 'Dirty Sock'],
+        [0, 'Hair Pin'],
+        [0, 'Microphone']
+    ],
+        [
+            [1, 'Hair Pin'],
+            [1, 'Half-Eaten Apple'],
+            [1, 'Bowling Ball'],
+            [1, 'Toothpaste']
+        ])).toEqual([
+            [1, 'Bowling Ball'],
+            [0, 'Dirty Sock'],
+            [1, 'Hair Pin'],
+            [1, 'Half-Eaten Apple'],
+            [0, 'Microphone'],
+            [1, 'Toothpaste']
+        ])
+    expect(inventory([],
+        [
+            [2, 'Hair Pin'],
+            [3, 'Half-Eaten Apple'],
+            [67, 'Bowling Ball'],
+            [7, 'Toothpaste']
+        ])).toEqual(
+
+            [
+                [67, 'Bowling Ball'],
+                [2, 'Hair Pin'],
+                [3, 'Half-Eaten Apple'],
+                [7, 'Toothpaste']
+              ]
+        )
+})
--- exercise.js
+++ exercise.js
@@ -1,14 +1,42 @@
+/* *Instructions*
+    For this challenge you need to compare and update the inventory stored as a 2D array agaisnt a 2nd array when new inventory comes up.
+
+    The tasks you need to perform are the following:
+
+    Update current existing inventory item quantities in the 1st array
+    If an item cannot be found you need to add it to the first array including it's quantity
+
+    The returned array containing the updated inventory needs to be in alphabetically order.
+
+
+
+*/
+
 /** DO NOT CHANGE THE FUNCTION NAME **/
-const func = (c) => {
-    /* Only make changes below this comment */
-    
-    
-    /* Only make changes below this comment */
+const inventory = (currInven, newInven) => {
+    newInven.forEach(function(item) {
+        createOrUpdate(currInven, item);
+      });
+      // All inventory must be accounted for or you're fired!
+      return currInven;
 }
 
-
+function createOrUpdate(arr1, item) {
+    var index = -1;
+    while (++index < arr1.length) {
+      if (arr1[index][1] === item[1]) {
+        arr1[index][0] += item[0];
+        return;
+      }
+      if (arr1[index][1] > item[1]) {
+        break;
+      }
+    }
+    arr1.splice(index, 0, item);
+  }
+  
 
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
-module.exports.func = func;
+module.exports.inventory = inventory;
