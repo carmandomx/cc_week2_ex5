650cea67e8aea357f0f1ba68eb1c47c5ba165fa4
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-03-14 15:05:05 -0600

    cc_week2_ex5 AndreiSantos

--- exercise.js
+++ exercise.js
@@ -14,29 +14,38 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const inventory = (currInven, newInven) => {
-    newInven.forEach(function(item) {
-        createOrUpdate(currInven, item);
-      });
-      // All inventory must be accounted for or you're fired!
-      return currInven;
-}
 
-function createOrUpdate(arr1, item) {
-    var index = -1;
-    while (++index < arr1.length) {
-      if (arr1[index][1] === item[1]) {
-        arr1[index][0] += item[0];
-        return;
-      }
-      if (arr1[index][1] > item[1]) {
-        break;
-      }
-    }
-    arr1.splice(index, 0, item);
-  }
+  let tempArr = [];
+
+  // forEach 1 used to add all elements[1] of currInven to a new arrar
+  currInven.forEach(element => {
+    tempArr.push(element[1]);
+  });
+
+  // forEach 2 used to add all elements[1] of newInven to a tampArr only if element[1]
+  // is not in tempArr
+  newInven.forEach(element => {
+    tempArr.includes(element[1]) ? null : tempArr.push(element[1]);
+  });
   
+  // sort tempArr and add 0 as value in element[0] for each element in tempArr and add it
+  // on a new array called answer
+  let answer = tempArr.sort().map( element => ([0, element]));
 
+  // forEach 3 used to compare all elements of answer with elements on currInven and newInven
+  answer.forEach( element => {
 
+    // forEach 4 and 5 used to change the value of each element[0] of answer and add their 
+    // quantity amount
+    currInven.forEach( current => {
+      current[1] === element[1] ? element[0]+=current[0] : null
+    })
+    newInven.forEach( newInv => {
+      newInv[1] === element[1] ? element[0]+=newInv[0] : null
+    })
+  })
+  return answer
+}
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.inventory = inventory;
