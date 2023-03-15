173fbac16aeaeab12eb0661262ce97333ddd30af
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-14 23:26:47 -0600

    ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -14,26 +14,55 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const inventory = (currInven, newInven) => {
-    newInven.forEach(function(item) {
-        createOrUpdate(currInven, item);
-      });
+  let arrtitems = [];
+  let arrnewitems = [];
+  let arrnewprices = [];
+
+  for(let i=0;i<currInven.length;i++){
+    let itemname = currInven[i];
+    arrtitems.push(itemname[1]);
+  }
+  for(let i=0;i<newInven.length;i++){
+    let newitemname = newInven[i];
+    arrnewitems.push(newitemname[1]);
+    arrnewprices.push(newitemname[0]);
+  }
+  for(let i=0;i<currInven.length;i++){
+    for(let j=0;j<newInven.length;j++){
+      let item = currInven[i];
+      let itemNew = newInven[j];
+      if(item[1]==itemNew[1]){
+        let itemsuma=item[0]+itemNew[0];
+        item[0]=itemsuma;
+        currInven[i]=item;
+      }
+    }
+  }
+  for(let a=0;a<arrnewitems.length;a++){
+    if(arrtitems.includes(arrnewitems[a])){}else{
+      let newcreateditem = [arrnewprices[a],arrnewitems[a]]
+      currInven.push(newcreateditem);
+
+    }
+  }
+  for(let a=0;a<currInven.length;a++){
+    let item = currInven[a];
+    [item[0],item[1]] = [item[1],item[0]];
+    currInven.splice(a,1,item)
+    }
+
+  currInven.sort();
+  for(let a=0;a<currInven.length;a++){
+      let item = currInven[a];
+      [item[0],item[1]] = [item[1],item[0]];
+      currInven.splice(a,1,item)
+      }
+  
+
       // All inventory must be accounted for or you're fired!
       return currInven;
 }
 
-function createOrUpdate(arr1, item) {
-    var index = -1;
-    while (++index < arr1.length) {
-      if (arr1[index][1] === item[1]) {
-        arr1[index][0] += item[0];
-        return;
-      }
-      if (arr1[index][1] > item[1]) {
-        break;
-      }
-    }
-    arr1.splice(index, 0, item);
-  }
   
 
 
